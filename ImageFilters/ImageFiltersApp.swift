e7dccc8c2414a8bf0bf27f34846944131cd56810
Ola Lovholm <ola.lovholm@origo.oslo.kommune.no>
2025-08-25 12:35:09 +0200

    Initial Commit

---
+++ ImageFilters/ImageFiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFiltersApp.swift
+//  ImageFilters
+//
+//  Created by Ola Loevholm on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ImageFiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageFiltersTests/ImageFiltersTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFiltersTests.swift
+//  ImageFiltersTests
+//
+//  Created by Ola Loevholm on 25/08/2025.
+//
+
+import Testing
+@testable import ImageFilters
+
+struct ImageFiltersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
